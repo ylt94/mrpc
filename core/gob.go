c7533d8d80041290c131d9f903fa3ed4de19e4b0
yuanlitao <ltyuan@suntekcorps.com>
2022-07-20 18:21:27 +0800

    server

--- core/gob.go
+++ core/gob.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"encoding/gob"
 	"io"
+	"log"
 )
 
 type GobCodec struct {
@@ -18,7 +19,7 @@ var _ Codec = (*GobCodec)(nil)
 
 func NewGobCodec(conn io.ReadWriteCloser) Codec {
 	buf := bufio.NewWriter(conn)
-	return GobCodec{
+	return &GobCodec{
 		conn: conn,
 		buf:  buf,
 		dec:  gob.NewDecoder(conn),
@@ -27,13 +28,33 @@ func NewGobCodec(conn io.ReadWriteCloser) Codec {
 }
 
 func (c *GobCodec) ReadHeader(h *Header) error {
-	return nil
+	return c.dec.Decode(h)
 }
 
 func (c *GobCodec) ReadBody(body interface{}) error {
+	return c.dec.Decode(body)
+}
+
+func (c *GobCodec) Write(h *Header, body interface{}) (err error) {
+	defer func() {
+		_ = c.buf.Flush()
+		if err != nil {
+			_ = c.Close
+		}
+	}()
+
+	if err := c.enc.Encode(h); err != nil {
+		log.Println("rpc codec: gob error encoding header:", err)
+		return err
+	}
+
+	if err := c.enc.Encode(body); err != nil {
+		log.Println("rpc codec: gob error encoding body:", err)
+		return err
+	}
 	return nil
 }
 
-func (c *GobCodec) Write(h *Header, body interface{}) error {
-	return nil
+func (c *GobCodec) Close() error {
+	return c.conn.Close()
 }
---
+++ mrpc/server.go
@@ -0,0 +1,141 @@
+package mrpc
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"reflect"
+	"sync"
+
+	"github.com/ylt94/mrpc/core"
+)
+
+const MagicNumber = 0x3bef5c
+
+type Option struct {
+	MagicNumber int
+	CodecType   core.Type
+}
+
+var DefaultOption = &Option{
+	MagicNumber: MagicNumber,
+	CodecType:   core.GobType,
+}
+
+type Server struct{}
+
+func NewServer() *Server {
+	return &Server{}
+}
+
+func Accept(lis net.Listener) { DefaultServer.Accept(lis) }
+
+var DefaultServer = NewServer()
+
+func (server *Server) Accept(lis net.Listener) {
+	for {
+		conn, err := lis.Accept()
+		if err != nil {
+			log.Println("rpc server: accept error:", err)
+			return
+		}
+		go server.ServerConn(conn)
+	}
+}
+
+func (server *Server) ServerConn(conn io.ReadWriteCloser) {
+	defer func() {
+		_ = conn.Close()
+	}()
+
+	var opt Option
+	if err := json.NewDecoder(conn).Decode(&opt); err != nil {
+		log.Println("rpc server: options json decode error", err)
+		return
+	}
+
+	if opt.MagicNumber != MagicNumber {
+		log.Printf("rpc server: invalid magic number %x", opt.MagicNumber)
+		return
+	}
+
+	f := core.NewCodecFuncMap[opt.CodecType]
+	if f == nil {
+		log.Printf("rpc server: invalid codec type %s", opt.CodecType)
+		return
+	}
+
+	server.serverCodec(f(conn))
+}
+
+var invalidRequest = struct{}{}
+
+func (server *Server) serverCodec(cc core.Codec) {
+	sending := new(sync.Mutex)
+	wg := new(sync.WaitGroup)
+
+	for {
+		req, err := server.readRequest(cc)
+		if err != nil {
+			if req == nil {
+				break
+			}
+			req.h.Error = err.Error()
+			server.sendResponse(cc, req.h, invalidRequest, sending)
+		}
+		wg.Add(1)
+		go server.handleRequest(cc, req, sending, wg)
+	}
+	wg.Wait()
+	_ = cc.Close()
+}
+
+type request struct {
+	h            *core.Header
+	argv, replyv reflect.Value
+}
+
+func (server *Server) readRequestHeader(cc core.Codec) (*core.Header, error) {
+	var h core.Header
+	if err := cc.ReadHeader(&h); err != nil {
+		if err != io.EOF && err != io.ErrUnexpectedEOF {
+			log.Println("rpc server: read header error:", err)
+		}
+		return nil, err
+	}
+	return &h, nil
+}
+
+func (server *Server) readRequest(cc core.Codec) (*request, error) {
+	h, err := server.readRequestHeader(cc)
+	if err != nil {
+		return nil, err
+	}
+	req := &request{h: h}
+
+	req.argv = reflect.New(reflect.TypeOf(""))
+	if err = cc.ReadBody(req.argv.Interface()); err != nil {
+		log.Println("rpc server: read argv err:", err)
+		return nil, err
+	}
+	return req, nil
+}
+
+func (server *Server) sendResponse(cc core.Codec, h *core.Header, body interface{}, sending *sync.Mutex) {
+	sending.Lock()
+	defer sending.Unlock()
+	if err := cc.Write(h, body); err != nil {
+		log.Println("rpc server: write response error:", err)
+	}
+}
+
+func (server *Server) handleRequest(cc core.Codec, req *request, sending *sync.Mutex, wg *sync.WaitGroup) {
+	// TODO, should call registered rpc methods to get the right replyv
+	// day 1, just print argv and send a hello message
+	defer wg.Done()
+	log.Println(req.h, req.argv.Elem())
+	req.replyv = reflect.ValueOf(fmt.Sprintf("geerpc resp %d", req.h.Seq))
+	server.sendResponse(cc, req.h, req.replyv.Interface(), sending)
+}

47a63a91fb875a1dd067e2edb0aac3ec756b0862
yuanlitao <ltyuan@suntekcorps.com>
2022-07-25 17:50:54 +0800

    client

---
+++ client/client.go
@@ -0,0 +1,95 @@
+package client
+
+import (
+	"errors"
+	"io"
+	"sync"
+
+	"github.com/ylt94/mrpc/core"
+	"github.com/ylt94/mrpc/mrpc"
+)
+
+type Call struct {
+	Seq           uint64
+	ServiceMethod string
+	Args          interface{}
+	Reply         interface{}
+	Error         error
+	Done          chan *Call
+}
+
+func (call *Call) done() {
+	call.Done <- call
+}
+
+type Client struct {
+	cc       core.Codec
+	opt      *mrpc.Option
+	sending  sync.Mutex
+	header   core.Header
+	mu       sync.Mutex
+	seq      uint64
+	pending  map[uint64]*Call
+	closing  bool
+	shutdown bool
+}
+
+var _ io.Closer = (*Client)(nil)
+
+var ErrShutdown = errors.New("connection is shut down")
+
+func (client *Client) Close() error {
+	client.mu.Lock()
+	defer client.mu.Unlock()
+
+	if client.closing {
+		return ErrShutdown
+	}
+
+	client.closing = true
+	return client.cc.Close()
+}
+
+func (client *Client) IsAvailable() bool {
+	client.mu.Lock()
+	defer client.mu.Unlock()
+
+	return !client.shutdown && !client.closing
+}
+
+func (client *Client) registerCall(call *Call) (uint64, error) {
+	client.mu.Lock()
+	defer client.mu.Unlock()
+
+	if client.closing || client.shutdown {
+		return 0, ErrShutdown
+	}
+
+	call.Seq = client.seq
+	client.pending[call.Seq] = call
+	client.seq++
+	return call.Seq, nil
+}
+
+func (client *Client) removeCall(seq uint64) *Call {
+	client.mu.Lock()
+	defer client.mu.Unlock()
+
+	call := client.pending[seq]
+	delete(client.pending, seq)
+
+	return call
+}
+
+func (client *Client) terminateCalls(err error) {
+	client.sending.Lock()
+	defer client.sending.Unlock()
+
+	client.mu.Lock()
+	defer client.mu.Unlock()
+
+	for _, call := range client.pending {
+		call.Error = err
+		call.done()
+	}
+}

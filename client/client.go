651a9b148440a06572c99682b3f9872fe5603803
yuanlitao <ltyuan@suntekcorps.com>
2022-07-26 17:52:19 +0800

    client

--- client/client.go
+++ client/client.go
@@ -1,9 +1,12 @@
 package client
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
+	"log"
+	"net"
 	"sync"
 
 	"github.com/ylt94/mrpc/core"
@@ -99,18 +102,21 @@ func (client *Client) recevie() {
 	var err error
 	for err == nil {
 		var h core.Header
-		//call 存在，但服务端处理出错，即 h.Error 不为空
+		//解析header出错
 		if err = client.cc.ReadHeader(&h); err != nil {
 			break
 		}
 		call := client.removeCall(h.Seq)
 		switch {
+		//call 不存在，可能是请求没有发送完整，或者因为其他原因被取消，但是服务端仍旧处理了。
 		case call == nil:
 			err = client.cc.ReadBody(nil)
+		//call 存在，但服务端处理出错，即 h.Error 不为空。
 		case h.Error != "":
 			call.Error = fmt.Errorf(h.Error)
 			err = client.cc.ReadBody(nil)
 			call.done()
+		//call 存在，服务端处理正常，那么需要从 body 中读取 Reply 的值。
 		default:
 			err = client.cc.ReadBody(call.Reply)
 			if err != nil {
@@ -122,4 +128,116 @@ func (client *Client) recevie() {
 
 	// error occurs, so terminateCalls pending calls
 	client.terminateCalls(err)
+}
+
+func NewClient(conn net.Conn, opt *mrpc.Option) (*Client, error) {
+	f := core.NewCodecFuncMap[opt.CodecType]
+	if f == nil {
+		err := fmt.Errorf("invalid codec type %s", opt.CodecType)
+		log.Println("rpc client: codec error :", err)
+		return nil, err
+	}
+
+	if err := json.NewDecoder(conn).Decode(opt); err != nil {
+		log.Println("rpc client: options send error: ", err)
+		_ = conn.Close()
+		return nil, err
+	}
+
+	return NewClientCodec(f(conn), opt), nil
+}
+
+func NewClientCodec(cc core.Codec, opt *mrpc.Option) *Client {
+	client := &Client{
+		seq:     1,
+		cc:      cc,
+		opt:     opt,
+		pending: make(map[uint64]*Call),
+	}
+	go client.recevie()
+	return client
+}
+
+func parseOptions(opts ...*mrpc.Option) (*mrpc.Option, error) {
+	if len(opts) == 0 || opts[0] == nil {
+		return mrpc.DefaultOption, nil
+	}
+
+	if len(opts) != 1 {
+		return nil, errors.New("number of options is more than 1")
+	}
+
+	opt := opts[0]
+	opt.MagicNumber = mrpc.DefaultOption.MagicNumber
+	if opt.CodecType == "" {
+		opt.CodecType = mrpc.DefaultOption.CodecType
+	}
+	return opt, nil
+}
+
+func Dail(network, address string, opts ...*mrpc.Option) (client *Client, err error) {
+	opt, err := parseOptions(opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := net.Dial(network, address)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		if client == nil {
+			_ = conn.Close()
+		}
+	}()
+
+	return NewClient(conn, opt)
+}
+
+func (client *Client) send(call *Call) {
+	client.sending.Lock()
+	defer client.sending.Unlock()
+
+	seq, err := client.registerCall(call)
+	if err != nil {
+		call.Error = err
+		call.done()
+		return
+	}
+
+	client.header.ServiceMethod = call.ServiceMethod
+	client.header.Seq = seq
+	client.header.Error = ""
+
+	if err := client.cc.Write(&client.header, call.Args); err != nil {
+		call := client.removeCall(seq)
+
+		if call != nil {
+			call.Error = err
+			call.done()
+		}
+	}
+}
+
+func (client *Client) Go(serviceMethod string, args, reply interface{}, done chan *Call) *Call {
+	if done == nil {
+		done = make(chan *Call, 10)
+	} else if cap(done) == 0 {
+		log.Panic("rpc client: done channel is unbuffered")
+	}
+
+	call := &Call{
+		ServiceMethod: serviceMethod,
+		Args:          args,
+		Reply:         reply,
+		Done:          done,
+	}
+	client.send(call)
+	return call
+}
+
+func (client *Client) Call(serviceMethod string, args, reply interface{}) error {
+	call := <-client.Go(serviceMethod, args, reply, make(chan *Call, 1)).Done
+	return call.Error
 }
